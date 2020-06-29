8446883e4461c60369783b096426a8a14cd7c4db
onee <244980740@qq.com>
2020-06-29 14:01:03 +0800

    '首次提交'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入viewUI
+import './plugins/iview.js'
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div >
+   
+    
+    <Button type="info">取人</Button>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Button } from 'view-design'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components:{
+    Button
   }
+  
 }
 </script>
